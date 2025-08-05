761eb6c5a2c269559dc8ef37cccbf336443d2c32
sjiya114 <sjiya04072004@gmail.com>
2025-08-05 13:16:01 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use("/api/user",user);
 app.get("/api",(req,res)=>{
     res.send("Backend API is working");
 })
-module,exports=app;
+module.exports=app;
 // const PORT=3000;
 // app.listen(PORT,()=>{
 //     console.log(`app start running on http://localhost:${PORT}`);
